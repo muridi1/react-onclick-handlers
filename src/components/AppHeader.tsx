f69202c02575e696c6fa05e83b1ca0be19c5693e
mohamed <muridi51@gmail.com>
2021-06-30 14:58:09 +0100

    added an on Click to the title which logs the title capitalised

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,8 +2,15 @@ interface AppHeaderProps {
   title: string;
 }
 
-function AppHeader({ title }: AppHeaderProps): JSX.Element {
-  return <h1>{title}</h1>;
-}
+ function AppHeader({ title }: AppHeaderProps): JSX.Element {
+
+    function handleClick() {
+      console.log(title.toUpperCase());
+    }
+    return (
+      <h1 onClick={handleClick}>{title}</h1>
+    );
+  }
+
 
 export default AppHeader;
