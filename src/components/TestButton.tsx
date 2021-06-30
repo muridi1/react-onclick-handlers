e809b54ee6116ffab015e2a623c86b99a058c24e
mohamed <muridi51@gmail.com>
2021-06-30 14:28:28 +0100

    changing test button to work

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,18 @@
-function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
-}
+// function TestButton(): JSX.Element {
+//   return (
+//     <button onClick={() => window.alert("Hello from button g!")}>
+//       Make me do something when I'm clicked...
+//     </button>
+//   )
+// }
 
+function TestButton(): JSX.Element {
+  function handleClick() {
+    console.log("Hello from button A!");
+  }
+
+  return (
+    <button onClick={handleClick}>TEst BUtton</button>
+  );
+}
 export default TestButton;
